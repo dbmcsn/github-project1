8308faa3f4f8527ac8cf66027754b0bd6660a652
dbmcsn <104976267+dbmcsn@users.noreply.github.com>
2022-11-03 16:52:54 +0800

    feat. update

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
         "ExpressJS",
         "NodeJS",
         "MongoDB (Mongoose)",
-       Link: "https://debm-book-review.herokuapp.com/login"
+       Link: "https://debm-book-review.herokuapp.com/login",
       ],
     },
   ]);
